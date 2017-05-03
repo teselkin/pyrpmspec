42b0dca6f213907322e8caab6acf8c2e217f0a4e
Dmitry Teselkin <dteselkin@mirantis.com>
2017-05-03 17:01:52 +0300

    Add simple test

---
+++ tests/test-parser.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from pyrpmspec.rpm import RpmSpecParser
+
+import os
+import shutil
+import yaml
+import wget
+
+test_spec = 'http://pkgs.fedoraproject.org/cgit/rpms/postgresql.git/plain/postgresql.spec'
+
+if os.path.exists('tmp'):
+    shutil.rmtree('tmp')
+os.mkdir('tmp')
+wget.download(test_spec, out='tmp')
+
+parser = RpmSpecParser()
+parsed = parser.parse('tmp/postgresql.spec')
+for spec in parsed:
+    print(yaml.dump(spec.dump(),
+                    default_style='',
+                    default_flow_style=False))
